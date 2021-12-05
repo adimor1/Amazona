454e1f52b6c50cf64a96e880d778b2bb83a14e56
עדי מור <206556755@idf.il>
2021-12-06 00:04:53 +0200

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from 'express';
 //import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path';
-//import dotenv from 'dotenv'; 
+import dotenv from 'dotenv'; 
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
 
-//dotenv.config(); 
+dotenv.config(); 
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
